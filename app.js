f5ed52c5341fd17b8cbcc93848818fc92524114b
JarekZ92 <z.jarek92@gmail.com>
2018-11-22 11:11:13 +0100

    added some properties into array

--- app.js
+++ app.js
@@ -1,1 +1,11 @@
 
+function group() {
+    const cities = [
+        
+            {name: 'New York', size: '1213km'},
+            {name: 'Warsaw', size: '517km'},
+            {name: 'Szanghaj', size: '5341km'}
+    ]
+
+    
+}
